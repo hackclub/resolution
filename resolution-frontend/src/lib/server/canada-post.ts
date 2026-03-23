8234a402912c67b2bc6d273ed3ecfae5057b4b90
Jenin <jenin@hackclub.com>
2026-03-23 09:10:56 -0400

    debug: log shipment XML to diagnose non-delivery handling error

--- resolution-frontend/src/lib/server/canada-post.ts
+++ resolution-frontend/src/lib/server/canada-post.ts
@@ -445,6 +445,7 @@ export async function createShipment(params: {
 	const { baseUrl, authHeader, customerNumber } = getCanadaPostConfig();
 
 	const shipmentXml = buildCreateShipmentXml(params);
+	console.log('Canada Post Shipment XML:', shipmentXml);
 
 	const cpEndpoint = `${baseUrl}/rs/${customerNumber}/${customerNumber}/shipment`;
 
