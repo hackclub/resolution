c1f2f7b252e042c5a19ce1555216a2f475603935
Jenin <jenin@hackclub.com>
2026-03-23 21:48:48 -0400

    debug: remove options block for non-contract to test if small packet rejects them

--- resolution-frontend/src/lib/server/canada-post.ts
+++ resolution-frontend/src/lib/server/canada-post.ts
@@ -164,7 +164,7 @@ export function buildCreateShipmentXml(params: {
 				<postal-zip-code>${(order.postalCode ?? '').replace(/\s/g, '').toUpperCase()}</postal-zip-code>
 			</address-details>
 		</destination>
-		${order.country !== 'CA' ? `<options>
+		${order.country !== 'CA' && contractId ? `<options>
 			<option>
 				<option-code>RTS</option-code>
 			</option>
